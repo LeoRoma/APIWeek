f13c6ec5af3f36fa8395b677fb7d875cff7c6c39
LeoRoma <xiajtn@gmail.com>
2020-08-05 14:06:24 +0100

    CatAPI project started

---
+++ homework/CatAPI/CatAPIController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatAPIController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
