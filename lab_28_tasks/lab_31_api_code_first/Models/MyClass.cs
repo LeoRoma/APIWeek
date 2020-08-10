06ff49047017868b92d3fe139afca23b1716327b
LeoRoma <xiajtn@gmail.com>
2020-08-10 10:27:39 +0100

    lab_31_api_code_first added

---
+++ lab_28_tasks/lab_31_api_code_first/Models/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_31_api_code_first.Models
+{
+    public class MyClass
+    {
+        public int MyClassId { get; set; }
+        public string FieldName1 { get; set; }
+
+    }
+}
---
+++ lab_28_tasks/lab_31_api_code_first/Models/MyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_31_api_code_first.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        
+        // Create table name MyClasses (plural) from MyClass(singular)
+        public DbSet<MyClass> MyClasses { get; set; }
+        public MyDbContext() { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<MyClass>(item =>
+            {
+                item.Property(myclass => myclass.MyClassId).IsRequired();
+            });
+            builder.Entity<MyClass>().HasData(new MyClass { MyClassId = 1, FieldName1 = "Some data" });
+        }
+    }
+}
