12ee66c1e828a2b06aa0fea05ee1cdb1fa110368
LeoRoma <xiajtn@gmail.com>
2020-08-10 18:24:19 +0100

    music project added

---
+++ homework/MusiAPIEntityCodeFirst/MusiAPIEntityCodeFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using MusiAPIEntityCodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MusiAPIEntityCodeFirst
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            using(var db = new MusicContext())
+            {
+                List<Artist> artists = new List<Artist>();
+                List<Song> songs = new List<Song>();
+
+                artists = db.Artists.ToList();
+                songs = db.Songs.ToList();
+
+                artists.ForEach(artist => Console.WriteLine($"Name: {artist.ArtistName}"));
+                songs.ForEach(song => Console.WriteLine($"Song title: {song.Title}"));
+            }
+        }
+    }
+}
