4837f5b412a3bbcbb5cd42a5b95d44e0e1e2d0e9
LeoRoma <xiajtn@gmail.com>
2020-08-21 15:23:44 +0100

    azure!

--- API/api-northwind-entity-core/api-northwind-entity-core/Startup.cs
+++ API/api-northwind-entity-core/api-northwind-entity-core/Startup.cs
@@ -27,9 +27,9 @@ namespace api_northwind_entity_core
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext <NorthwindContext> (options => options.UseSqlServer(Configuration.GetConnectionString("Northwind")));
-            services.AddDbContext<NorthwindContext>(options => options.UseSqlite("Data Source = Northwind.db"));
-
+            services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Northwind")));
+            //services.AddDbContext<NorthwindContext>(options => options.UseSqlite("Data Source = Northwind.db"));
+            //services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Data Source=helloworld9000.database.windows.net;Initial Catalog=spartaDb;User ID=hello9000admin;Password=Pa44word;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")));
             services.AddControllers();
         }
 
